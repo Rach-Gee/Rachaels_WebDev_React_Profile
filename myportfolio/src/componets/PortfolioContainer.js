1a82fe279a6da8a223c12dcc915414962f61bd26
Rach Gee <Rach-Gee@hotmail.com>
2021-10-23 12:06:21 +0800

    adding About Me page

---
+++ myportfolio/src/componets/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Nav from './Navigation';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Blog from './pages/Blog';
+// import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('aboutMe');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <Home />;
+    }
+    if (currentPage === 'Protfolio') {
+      return <About />;
+    }
+    if (currentPage === 'Contact') {
+      return <Blog />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
---
+++ myportfolio/src/componets/pages/AboutMe.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h2 id="aboutme">About Me</h2>
+      <p>I am a driven, professional individual eager to break into web development.
+                Currently working as a Payroll Systems Analyst where I got my first taste of programming.
+                I had been working in my company for five years in Operations and Finance when an opportunity arose to
+                move
+                into a systems role,
+                I was officially hooked. This is where my love for computers, puzzle solving, and coding started.
+
+
+                Being the sole person in my
+                department to an 11 thousand employee large company meant I had to self-learn and learn quick!
+                Developing
+                live data,
+                large scale reports using various BI tools as well as SQL and PayGlobal builds. Three years on and my
+                passion for learning
+                has not haltered. Wanting to take on a new challenge, I commenced a web developer and coding bootcamp
+                with
+                UWA. But enough
+                about me, take a look as some of the things I have been creating by clicking the image.
+            </p>
+    </div>
+  );
+}
