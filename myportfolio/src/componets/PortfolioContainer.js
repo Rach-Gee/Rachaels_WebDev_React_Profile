9cc353c64a3f8b27a53afdb0622c3edfb0af13a9
Rach Gee <Rach-Gee@hotmail.com>
2021-10-23 12:10:58 +0800

    Bug fix

--- myportfolio/src/componets/PortfolioContainer.js
+++ myportfolio/src/componets/PortfolioContainer.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Nav from './Navigation';
-// import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
+import Home from './pages/Home';
 // import About from './pages/About';
 // import Blog from './pages/Blog';
 // import Contact from './pages/Contact';
@@ -11,15 +12,15 @@ export default function PortfolioContainer() {
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
     if (currentPage === 'AboutMe') {
-      return <Home />;
+      return <AboutMe />;
     }
-    if (currentPage === 'Protfolio') {
-      return <About />;
-    }
-    if (currentPage === 'Contact') {
-      return <Blog />;
-    }
-    return <Resume />;
+    // if (currentPage === 'Protfolio') {
+    //   return <About />;
+    // }
+    // if (currentPage === 'Contact') {
+    //   return <Blog />;
+    // }
+    // return <Resume />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
